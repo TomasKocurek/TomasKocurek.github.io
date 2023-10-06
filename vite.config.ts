fa7eef14ecbed57bba762b1a0f27d7abb1741100
TomasKocurek <tomas.kocurek2000@gmail.com>
2023-10-06 21:32:49 +0200

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/TomasKocurek/TomasKocurek.github.io",
   plugins: [react()],
 });
